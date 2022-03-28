d96a4d28ee373895f3c10f465021c80a89f1ed60
Hanifa Fauziah <80946219+hanifaafauziah@users.noreply.github.com>
2022-03-28 11:55:23 +0700

    using map

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ export default function App() {
   return (
     <div className="App">
       <h1 className="title">Spotify Playlist</h1>
-      <Music
+      <div className="wrapper">
+        {data.map((e) => {
+          return (
+            <Music
+              key={e.id}
+              album={e.album.images[1].url}
+              name={e.name}
+              artist={e.artists[0].name}
+              url={e.external_urls.spotify}
+            />
+          );
+        })}
+      </div>
+      {/* <Music
         name={data.name}
         artist={data.album.artists[0].name}
         album={data.album.images[1].url}
         url={data.external_urls.spotify}
-      />
+      /> */}
     </div>
   );
 }
